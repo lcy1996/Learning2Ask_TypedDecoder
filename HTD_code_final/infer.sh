0e19d3555fe4cf25d8698bee06e1a62b454ce3dd
lcy1996 <992195697@qq.com>
2018-04-27 17:17:47 +0800

    Add HTD and STD code

---
+++ HTD_code_final/infer.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=3 python main.py --is_train False --inference_path weibo_pair_test_Q.post --inference_version 200000 --batch_size 1 --keywords_per_sentence 20
---
+++ HTD_code_final/train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=2 python main.py --question_data=True --per_checkpoint=1000 --inference_version=0
